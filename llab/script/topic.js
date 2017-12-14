5a79b46e9661e08baddb096abd609e72ae46b23a
Michael Ball <cycomachead@gmail.com>
2017-12-14 00:47:13 -0800

    use jQuery

--- llab/script/topic.js
+++ llab/script/topic.js
@@ -241,7 +241,7 @@ llab.displayTopic = function() {
             success : llab.renderFull
         });
     } else {
-        document.getElementsByTagName(llab.selectors.FULL).item(0).innerHTML = "Please specify a file in the URL.";
+        $(llab.selectors.FULL).text("Please specify a file in the URL.");
     }
 }
 
