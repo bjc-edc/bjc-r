ce8d1417b240f1a2cf197cfb2c40dd578fe3231d
Mary Fries <mfries@edc.org>
2020-07-05 15:39:27 -0400

    Design Update: New CSS and index
    updated CSS for all pages, new landing page, added fontawesome fonts, moved old mainpage to /old

--- llab.js
+++ llab.js
@@ -26,15 +26,8 @@ llab.llab_path = llab.rootURL + llab.install_directory;
 // reference your custom CSS files, from within llab install directory.
 // Multiple CSS files is fine, include a separate push for each
 llab.paths.css_files.push('css/default.css');
-llab.paths.css_files.push('../css/edcdevtech-headerfooter.css'); /* for new headers and footers, by EDC Dev Tech, May 2020 */
-// In /bjc-r/css
-// new CSS added for new mainpage only, by Mary 1/26/16
-var url = window.location.pathname;
-if (url.slice(url.search("/bjc-r/")) == "/bjc-r/" || url == "/") {
-    llab.paths.css_files.push('../css/bjc-mainpage.css');
-} else {
-    llab.paths.css_files.push('../css/bjc.css');
-}
+llab.paths.css_files.push('../css/bjc.css');
+llab.paths.css_files.push('../css/edcdevtech-headerfooter.css'); /* new headers & footers by EDC Dev Tech & modified by Mary, 05/2020 */
 
 
 // courses -- path to folder containing courses.
