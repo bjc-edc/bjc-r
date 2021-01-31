561c88f897496f4d9787292c46e7f1c9f7c03e9a
brianharvey <bh@cs.berkeley.edu>
2021-01-30 18:51:42 -0800

    finishing summary feature
    index needs hand-tweaking wrt capitalization, etc.

--- cur/programming/vocab.c
+++ cur/programming/vocab.c
@@ -6,6 +6,8 @@
 #include <sys/mman.h>
 #include <regex.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 size_t page_size;
 
@@ -31,7 +33,8 @@ int main(int argc, char **argv) {
     char *class=argv[1],*secp,*inp;
     char outname[100],searchstring[100],divtext[100],sect[8];
     char unitnum[4]="0",h3[100],h2[100],units[300],link[300];
-    int fin,fout,funit,i,len,depth,first=1,firstpage=1,vocab=0,boxnum;
+    int fin,fout,findex,funit;
+    int bflag,i,len,depth,first=1,firstpage=1,vocab=0,boxnum;
     char *mem,*startp,*endp,*nextp,*foop,*bazp;
     FILE *fp;
     char ch;
@@ -52,6 +55,8 @@ int main(int argc, char **argv) {
     else if (!strcmp(argv[1],"exam")) secp=" AP Exam Hints";
     else if (!strcmp(argv[1],"assessment-data")) secp=" Self-Tests";
     else secp=" Summary";
+    findex=open("summaries/index-presort",O_CREAT|O_WRONLY,0744);
+    (void)lseek(findex,0L,2);
     fout=creat(outname,0744);
     write(fout,intro,strlen(intro));
     write(fout,unitnum,strlen(unitnum));
@@ -60,7 +65,6 @@ int main(int argc, char **argv) {
     sprintf(searchstring,"<div class=\"%s",class);
     sprintf(divtext,"<div class=\"%s100Width\" ",class);
     for (i=2;i<argc;i++) {		/* for each input file */
-	boxnum=0;
 	fin=open(argv[i],O_RDONLY);
 	secp=sect;
 	*secp++ = argv[i][0];		/* sect <- "u.l.p" from filename */
@@ -88,6 +92,7 @@ int main(int argc, char **argv) {
 	    (void)write(fout,h2,strlen(h2));
 	}
 	if (!strcmp(secp-2,".1")) {	/* if first page of new lab */
+	    boxnum=0;
 	    endp=strstr(mem,"<title>");
 	    foop=strstr(endp,",");
 	    sprintf(h3,"<h3>%.*s</h3>\n%c",(int)(foop-(endp+14)),endp+14,'\0');
@@ -135,6 +140,28 @@ int main(int argc, char **argv) {
 	    endp += 4;	    /* for the "div>" */
 	    (void)write(fout,startp,endp-startp);
 	    (void)write(fout,"\n",1);
+	    if (vocab) {
+		while (startp<endp) {
+		    foop = strstr(startp,"<strong>");
+		    bazp = strstr(startp,"<b>");
+		    if (bazp != NULL && bazp < foop) {
+			foop = bazp;
+		    }
+		    if (foop < endp && foop != NULL) {
+			bflag = (foop[1] == 'b');
+			bazp = strstr(foop, (bflag ? "</b>" : "</strong>"));
+			foop = foop + (bflag ? 3 : 8);
+			if (*foop == ':') foop += 2;
+			(void)write(findex,foop,bazp-foop);
+			startp = bazp;
+			(void)sprintf(link," <a href=\"/bjc-r/cur/programming/summaries/%s#box%d\" title=\"/bjc-r/cur/programming/summaries/%s#box%d\">%s</a>\n%c",
+				      outname,boxnum,outname,boxnum,sect,'\0');
+			(void)write(findex,link,strlen(link));
+		    } else {
+			startp = endp;
+		    }
+		}
+	    }
 	}
 	close(fin);
 	/* no more vocab boxes found in this file */
@@ -142,4 +169,5 @@ int main(int argc, char **argv) {
     }
     write(fout,outro,strlen(outro));
     close(fout);
+    close(findex);
 }
