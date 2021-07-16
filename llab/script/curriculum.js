54d390fa6404616f6529cd06d899fdedbc23605b
Michael Ball <cycomachead@gmail.com>
2021-07-16 16:13:39 -0700

    use window.load to hide dev comments

--- llab/script/curriculum.js
+++ llab/script/curriculum.js
@@ -377,10 +377,12 @@ llab.toggleDevComments = function() {
 };
 
 llab.hideAllDevComments = function() {
+  alert('HIDE ALL')
   $('.todo, .comment, .commentBig').hide();
 }
 
 llab.canShowDevComments = function () {
+  return false;
   return ['localhost', '127.0.0.1'].includes(window.location.hostname);
 }
 
@@ -393,8 +395,9 @@ llab.createTitleNav = function() {
     .click(llab.toggleDevComments)
     .text('Toggle developer todos/comments (red boxes)');
   } else {
+    alert('DEV COMMENTS SHOULD NOT BE SHOWN')
     // Delay hiding until everything is ready. (Shouldn't be needed?)
-    $(document).ready(llab.hideAllDevComments);
+    $(window).load(llab.hideAllDevComments);
   }
 
   // The BJC Logo takes you to the course ToC, or the BJC index when there is no course defined.
