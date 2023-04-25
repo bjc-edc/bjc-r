22b18910a8b3cc058d46e1da2b385d53595a0cea
Michael Ball <michael@mball.co>
2023-04-25 15:07:02 -0700

    Add disclosure indicators to open/close sections

--- llab/loader.js
+++ llab/loader.js
@@ -5,7 +5,7 @@
 
 
 const THIS_FILE = 'loader.js';
-const RELEASE_DATE = '2021-12-07';
+const RELEASE_DATE = '2023-04-25';
 
 // Basic llab shape.
 // TODO: We should separate this out a little more...
