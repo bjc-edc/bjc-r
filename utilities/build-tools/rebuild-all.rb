3dfa57a0aee89436ca3827df32458e3bb270eb39
Michael Ball <michael@mball.co>
2023-06-09 11:12:34 -0700

    Add english to script runner

--- utilities/build-tools/rebuild-all.rb
+++ utilities/build-tools/rebuild-all.rb
@@ -8,5 +8,8 @@ require_relative 'main'
 
 puts "Rebuilding all Index/summaries from: #{ENV['PWD']}"
 
+enRunner = Main.new(root: ENV['PWD'], cur_dir: "programming", topic_dir: "nyc_bjc", language: "en")
+enRunner.Main
+
 esRunner = Main.new(root: ENV['PWD'], cur_dir: "programming", topic_dir: "nyc_bjc", language: "es")
 esRunner.Main
