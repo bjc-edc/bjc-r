f155e63c79e249d30a9308df9109bb00c4d68bd9
Michael Ball <michael@mball.co>
2023-06-19 03:37:57 -0700

    Add a helpers file

---
+++ utilities/build-tools/bjc_helpers.rb
@@ -0,0 +1,12 @@
+module  BJCHelpers
+  def language_ext(lang)
+    lang == 'en' ? '' : ".#{lang}"
+  end
+
+  def url_to_path(url, root: '')
+
+  end
+
+  def path_to_url(path, root: '')
+  end
+end
