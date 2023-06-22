4a337933b7d308e53bb7e1489ad35fc1b94efa93
Michael Ball <michael@mball.co>
2023-06-22 03:01:35 -0700

    The thing seems to parse a file, but does not really save the data

--- utilities/build-tools/topic.rb
+++ utilities/build-tools/topic.rb
@@ -4,14 +4,22 @@ class BJCTopic
   attr_reader :file_path, :file_name, :title, :language
 
   # TODO: Is it useful to know the course a topic came with?
-  def initialize(path, course: nil, language: 'en'); end
+  def initialize(path, course: nil, language: 'en')
+    @file_path = path
+  end
+
+  def file_contents
+    @file_contents ||= File.read(@file_path)
+  end
 
   # This should return some hash-type structure
   # look at the code in llab
   # TODO: this could arguably be its own class.
   # Is this all that's needed (recursively) ?
   # { title, type, content, number, pages: [] }
-  def parse; end
+  def parse
+    parse_topic_file(file_contents)
+  end
 
   def unit_number; end
 
@@ -43,16 +51,23 @@ class BJCTopic
   #### CODE BELOW THIS LINE SHOULD BE REWRITTEN!!!
   ### This was JavaScript (mostly) auto-translated by ChatGPT
   #########
-  llab = {}
-  llab[:topic_keywords] = {}
-  llab[:topic_keywords][:resources] = %w[quiz assignment resource
-                                         forum video extresource
-                                         reading group]
-  llab[:topic_keywords][:headings] = %w[h1 h2 h3 h4 h5 h6 heading]
-  llab[:topic_keywords][:info] = %w[big-idea learning-goal]
+
+  # TODO: write this in ruby
+  def strip_comments(s)
+    s
+  end
 
   # TODO: comment...
   def parse_topic_file(data)
+    # TEMPORARY HACK
+    llab = {}
+    llab[:topic_keywords] = {}
+    llab[:topic_keywords][:resources] = %w[quiz assignment resource
+                                           forum video extresource
+                                           reading group]
+    llab[:topic_keywords][:headings] = %w[h1 h2 h3 h4 h5 h6 heading]
+    llab[:topic_keywords][:info] = %w[big-idea learning-goal]
+
     llab[:file] = llab[:topic]
 
     data = data.gsub(/(\r)/, '') # normalize line endings
@@ -62,13 +77,10 @@ class BJCTopic
     topic_model = nil
     section = nil
     raw = false
-    # url = document.URL
-
     i = 0
     while i < lines.length
       # TODO: remove text after // in a line.
-      # line = llab.strip_comments(lines[i])
-      line = line.strip
+      line = strip_comments(lines[i]).strip
 
       unless raw
         if line.length.positive?
@@ -83,32 +95,32 @@ class BJCTopic
             topics[:topics].push(topic_model)
             section = { title: '', contents: [], type: 'section' }
             topic_model[:contents].push(section)
-          elsif llab.is_heading(line)
-            heading_type = llab.get_keyword(line, llab[:topic_keywords][:headings])
+          elsif is_heading(line)
+            heading_type = get_keyword(line, llab[:topic_keywords][:headings])
             if section[:contents].length > 0
               section = { title: '', contents: [], type: 'section' }
               topic_model[:contents].push(section)
             end
-            section[:title] = llab.get_content(line)['text']
+            section[:title] = get_content(line)['text']
             section[:headingType] = heading_type
           elsif line[0] == '}'
             # shouldn't matter
-          elsif llab.is_info(line)
-            tag = llab.get_keyword(line, llab[:topic_keywords][:info])
-            indent = llab.indent_level(line)
-            content = llab.get_content(line)['text'] # ?
+          elsif is_info(line)
+            tag = get_keyword(line, llab[:topic_keywords][:info])
+            indent = indent_level(line)
+            content = get_content(line)['text'] # ?
             # TODO: do we really need indentation now?
             # if so, I think it should be added to the type
             # and only if indentation levels != nested levels.
-            item = { type: tag, contents: content, indent: }
+            item = { type: tag, contents: content, indent: indent }
             section[:contents].push(item)
-          elsif llab.is_resource(line) || true
+          elsif is_resource(line) || true
             # FIXME: dumb way to handle lines without a known tag
             # Shouldn't this just be an else case?
-            tag = llab.get_keyword(line, llab[:topic_keywords][:resources])
-            indent = llab.indent_level(line)
-            content = llab.get_content(line)
-            item = { type: tag, indent:, contents: content[:text],
+            tag = get_keyword(line, llab[:topic_keywords][:resources])
+            indent = indent_level(line)
+            content = get_content(line)
+            item = { type: tag, indent: indent, contents: content[:text],
                      url: content[:url] }
             section[:contents].push(item)
           end
@@ -118,18 +130,17 @@ class BJCTopic
 
         if raw
           raw_html = []
-          text = llab.get_content(line)['text'] # in case they start the raw html on the same line
+          text = get_content(line)['text'] # in case they start the raw html on the same line
           raw_html.push(text) if text
 
           # FIXME: -- if nested topics are good check for {
-          while lines[i + 1].length >= 1 && lines[i + 1][0] != '}' && !llab.is_keyword(lines[i + 1])
+          while lines[i + 1].length >= 1 && lines[i + 1][0] != '}' && !is_keyword(lines[i + 1])
             i += 1
             line = lines[i]
             raw_html.push(line)
           end
 
-          # FIXME: -- shouldn't the type have a - ?
-          section[:contents].push({ type: 'raw_html', contents: raw_html })
+          section[:contents].push({ type: 'raw-html', contents: raw_html })
           raw = false
         end
       end
@@ -142,15 +153,20 @@ class BJCTopic
     topics
   end
 
+  # TODO
+  def indent_level(s)
+    0
+  end
+
   def matches_array(line, array)
     matches = array.map { |s| line.match(s) }
     matches.any? { |m| !m.nil? }
   end
 
-  # TODO: comment...
   def get_keyword(line, array)
     matches = array.map { |s| line.match(s) }
-    array[which(matches.map { |m| !m.nil? })]
+    index = matches.index { |m| !m.nil? }
+    array[index] unless index.nil?
   end
 
   def get_content(line)
@@ -162,15 +178,17 @@ class BJCTopic
   end
 
   def is_resource(line)
-    matches_array(line, llab.topic_keywords.resources)
+    matches_array(line, %w[quiz assignment resource
+      forum video extresource
+      reading group])
   end
 
   def is_info(line)
-    matches_array(line, llab.topic_keywords.info)
+    matches_array(line, %w[big-idea learning-goal])
   end
 
   def is_heading(line)
-    matches_array(line, llab.topic_keywords.headings)
+    matches_array(line, %w[h1 h2 h3 h4 h5 h6 heading])
   end
 
   def is_keyword(line)
