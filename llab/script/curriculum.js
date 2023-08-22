6ef17fab1ecdad3c9ab5158f2fad2b5a1bbb84f5
Michael Ball <michael@mball.co>
2023-08-22 10:19:39 -0700

    Revert "move some JS around"
    This reverts commit 3a0fe7f6f3874a09fdfcaa938135c28362e62cea.

--- llab/script/curriculum.js
+++ llab/script/curriculum.js
@@ -99,15 +99,19 @@ llab.secondarySetUp = function() {
   if (llab.read_cache(llab.file)) {
     llab.processLinks(llab.read_cache(llab.file));
   } else {
-    fetch(`${llab.topics_path}/${llab.file}`)
-      .then(response => response.text())
-      .then(text => llab.processLinks(text))
-      .catch(err => {
-          console.warn('Something went wrong.', err);
-          if (typeof Sentry !== 'undefined') {
-              Sentry.captureException(err);
-          }
-      });
+    $.ajax({
+      url: `${llab.topics_path}/${llab.file}`,
+      type: "GET",
+      contentType: 'text/plain; charset=UTF-8',
+      dataType: "text",
+      cache: false,
+      success: llab.processLinks,
+      error: (_jqXHR, _status, error) => {
+        if (Sentry) {
+          Sentry.captureException(error);
+        }
+      }
+    });
   };
 }; // close secondarysetup();
 
@@ -116,7 +120,7 @@ llab.secondarySetUp = function() {
 *  and creates navigation buttons.
 *  FIXME: This should share code with llab.topic!
 */
-llab.processLinks = function(data) {
+llab.processLinks = function(data, _status, _jqXHR) {
   /* NOTE: DO NOT REMOVE THIS CONDITIONAL WITHOUT SERIOUS TESTING
   * llab.file gets reset with the ajax call.
   */
@@ -430,7 +434,9 @@ llab.dropdownItem = function(text, url) {
 // Pages directly within a lab. Excludes 'topic' and 'course' pages.
 llab.isCurriculum = function() {
   if (llab.getQueryParameter('topic')) {
-    return !llab.isTopicFile();
+    return ![
+      llab.empty_topic_page_path, llab.topic_launch_page, llab.alt_topic_page
+    ].includes(llab.stripLangExtensions(location.pathname));
   }
   return false;
 }
