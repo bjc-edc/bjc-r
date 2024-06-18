420fc1a8180502b783a4c0740edf34a97b2880b7
Michael Ball <michael@mball.co>
2024-06-18 15:49:12 -0700

    enhance output of specs

--- utilities/spec/accessibility_spec.rb
+++ utilities/spec/accessibility_spec.rb
@@ -3,7 +3,7 @@
 # Run accessibility specs for all pages in the webiste.
 # This runs the axe accessibility checker on each page in a headless browser.
 
-require 'nokogiri'
+  require 'nokogiri'
 
 # Load our custom BJC tools
 require_relative '../build-tools/bjc_helpers'
@@ -22,10 +22,11 @@ COURSES = %w[
 ]
 
 def load_site_urls(courses)
+  # Map is a course_name => [url1, url2, ...]
   courses.map do |course|
     puts "Buidling URLs for #{course}..."
-    load_all_urls_in_course("#{course}.html")
-  end.flatten
+    [course, load_all_urls_in_course("#{course}.html")]
+  end.to_h
 end
 
 def extract_urls_from_page(topic_file, course)
@@ -54,21 +55,18 @@ def load_all_urls_in_course(course)
 
   results << topic_pages
   results << urls.filter_map { |url| "#{url}&course=#{course}" if !url.match?(/\.topic/) }
-  results.flatten
+  results.flatten.reject { |u| !u.start_with?('/bjc-r') }.uniq
 end
 
-ALL_PAGES = []
+ALL_PAGES = {}
 # TODO: We need to figure out what things should be tested.
-# Loading all pages, including the old ones is not helpful.
-base_path = File.join(File.dirname(__FILE__), '..', '..')
-site = File.join(base_path, '**', 'index.html')
-index_pages = Dir.glob(site).filter_map { |f| f.gsub(base_path, '/bjc-r') if !f.match?(/old\//) }
+# base_path = File.join(File.dirname(__FILE__), '..', '..')
+# site = File.join(base_path, '**', 'index.html')
+# index_pages = Dir.glob(site).filter_map { |f| f.gsub(base_path, '/bjc-r') if !f.match?(/old\//) }
+# ALL_PAGES['Indexes'] = index_pages
+ALL_PAGES.merge!(load_site_urls(COURSES))
 
-ALL_PAGES.concat(index_pages)
-ALL_PAGES.concat(load_site_urls(COURSES)).reject! { |u| !u.start_with?('/bjc-r') }.uniq!
-
-puts "Running tests on #{ALL_PAGES.count} pages."
-# puts "  - #{ALL_PAGES.join("\n  - ")}\n#{'=' * 72}\n\n"
+puts "Running tests on #{ALL_PAGES.values.map(&:length).sum} pages."
 
 # Axe-core test standards groups
 # See https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#axe-core-tags
@@ -88,26 +86,39 @@ excluded_elements = [
   'var', # Snap! elements don't have enough color contrast.
 ]
 
+# ALL_PAGES is a hash of course names to arrays of URLs
+ALL_PAGES.each do |course, paths|
+  paths.each do |path|
+    fileted_path = path.gsub('/bjc-r/', '')
+    topic = fileted_path.match(/topic=(.*\.topic)/) ? Regexp.last_match(1) : 'no-topic'
+    # Trim all query parameters
+    fileted_path = fileted_path.split('?').first
 
-ALL_PAGES.each do |path|
-  describe "'#{path}' is accessible", type: :feature, js: true do
-    before(:each) do
-      visit(path)
+    # using course as a tag allows passing `--tag bjc4nyc` to rspec to run only the
+    # tests for that course.
+    describe "#{course} : #{topic} : #{fileted_path} is accessible",
+      type: :feature, js: true, course.to_sym => true do
+
+      before(:each) do
+        visit(path)
+        # Esure the response is OK
+      end
+
+      # These tests should always be enabled.
+      it 'according to WCAG 2.0 AA' do
+        expect(page).to be_axe_clean
+          .according_to(*required_a11y_standards, "#{path} does NOT meet WCAG 2.0 AA")
+          .skipping(*skipped_rules)
+          .excluding(*excluded_elements)
+      end
+
+      # TODO: This is commented out just to reduce the noise...
+      # it 'according to WCAG 2.2 and all additional standards', :skip do
+      #   expect(page).to be_axe_clean
+      #     .according_to(*complete_a11y_standards)
+      #     .skipping(*skipped_rules)
+      #     .excluding(*excluded_elements)
+      # end
     end
-
-    # These tests should always be enabled.
-    it 'according to WCAG 2.0 AA' do
-      expect(page).to be_axe_clean
-        .according_to(*required_a11y_standards, "#{path} does NOT meet WCAG 2.0 AA")
-        .skipping(*skipped_rules)
-        .excluding(*excluded_elements)
-    end
-
-    # it 'according to WCAG 2.2 and all additional standards', :skip do
-    #   expect(page).to be_axe_clean
-    #     .according_to(*complete_a11y_standards)
-    #     .skipping(*skipped_rules)
-    #     .excluding(*excluded_elements)
-    # end
   end
 end
